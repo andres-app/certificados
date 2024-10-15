906bf9a2e9f239855da6187b6f346c24aa42610f
Anndres <silvabasauri@gmail.com>
2024-10-15 16:25:09 -0500

    Se formatea la fecha para que sea DD/MM/AAAA

--- view/Certificado/certificado.js
+++ view/Certificado/certificado.js
@@ -13,39 +13,47 @@ $(document).ready(function(){
         
         // Verifica si total_horas está presente
         console.log(data);
-    
+
         /* Luego de verificar, continúa con el código de generación del certificado */
         image.src = data.cur_img;
         ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    
+
         ctx.font = '40px Arial';
         ctx.textAlign = "center";
         ctx.textBaseline = 'middle';
         var x = canvas.width / 2;
         ctx.fillText(data.usu_nom + ' ' + data.usu_apep + ' ' + data.usu_apem, x, 270);
-    
+
         ctx.font = '30px Arial';
         ctx.fillText(data.cur_nom, x, 380);
-    
-        // ctx.font = '18px Arial';
-        // ctx.fillText(data.inst_nom + ' ' + data.inst_apep + ' ' + data.inst_apem, x, 420);
-        // ctx.font = '15px Arial';
-        // ctx.fillText('Instructor', x, 450);
-    
+
+        // Formatear fechas a DD/MM/AAAA
+        const fechaInicio = formatearFecha(data.cur_fechini);
+        const fechaFin = formatearFecha(data.cur_fechfin);
+
         ctx.font = '15px Arial';
-        ctx.fillText('Desde el: ' + data.cur_fechini + ' / ' + 'Hasta el: ' + data.cur_fechfin, x, 490);
-    
+        ctx.fillText('Desde el: ' + fechaInicio + ' / ' + 'Hasta el: ' + fechaFin, x, 490);
+
         // Agregar horas del curso
         ctx.font = '15px Arial';
         ctx.fillText('Total de horas: ' + data.total_horas, x, 450);
-    
+
         imageqr.src = "../../public/qr/" + curd_id + ".png";
         ctx.drawImage(imageqr, 400, 500, 100, 100);
-    
+
         $('#cur_descrip').html(data.cur_descrip);
     });    
 
 });
+
+/* Función para formatear la fecha en formato DD/MM/AAAA */
+function formatearFecha(fecha) {
+    const dateObj = new Date(fecha);
+    const dia = ('0' + dateObj.getDate()).slice(-2);
+    const mes = ('0' + (dateObj.getMonth() + 1)).slice(-2); // Los meses en JavaScript son de 0-11
+    const anio = dateObj.getFullYear();
+    return `${dia}/${mes}/${anio}`;
+}
 
 /* Recarga por defecto solo 1 vez */
 window.onload = function() {
