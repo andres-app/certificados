16cbbb0f19a8f424d63dbe2691edda7afba9a45b
Anndres <silvabasauri@gmail.com>
2024-10-15 23:08:51 -0500

    Se realiza de manera dinamica la conexion para ambiente de desarrollo y produccion

--- config/conexion.php
+++ config/conexion.php
@@ -1,40 +1,51 @@
 <?php
-    /*TODO: Inicializando la sesion del usuario */
-    session_start();
+/*TODO: Inicializando la sesion del usuario */
+session_start();
 
-    /*TODO: Iniciamos Clase Conectar */
-    class Conectar{
-        protected $dbh;
+/*TODO: Iniciamos Clase Conectar */
+class Conectar{
+    protected $dbh;
 
-        /*TODO: Funcion Protegida de la cadena de Conexion */
-        protected function Conexion(){
-            try {
-                /*TODO: Cadena de Conexion Desarrollo*/
-				$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=certificados","root","");
-
-                /*TODO: Cadena de Conexion Produccion*/
-				//$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=u274409976_certificados","u274409976_certificados","Redes72607251$$$");
-				return $conectar;
-			} catch (Exception $e) {
-                /*TODO: En Caso hubiera un error en la cadena de conexion */
-				print "¡Error BD!: " . $e->getMessage() . "<br/>";
-				die();
-			}
-        }
-
-        /*TODO: Para impedir que tengamos problemas con las ñ o tildes */
-        public function set_names(){
-            return $this->dbh->query("SET NAMES 'utf8'");
-        }
-
-        /*TODO: Ruta principal del proyecto */
-        public static function ruta(){
-            //Desarrollo
-            return "http://localhost/certificados/";
+    /*TODO: Funcion Protegida de la cadena de Conexion */
+    protected function Conexion(){
+        try {
+            // Detectar si estamos en entorno de desarrollo o producción
+            $host = $_SERVER['HTTP_HOST'];
             
-            //Produccion
-            //return "https://peruaprende.com/certificados/";
+            if($host == 'localhost' || $host == '127.0.0.1') {
+                // Desarrollo
+                $conectar = $this->dbh = new PDO("mysql:host=localhost;dbname=certificados","root","");
+            } else {
+                // Producción
+                $conectar = $this->dbh = new PDO("mysql:host=localhost;dbname=u274409976_certificados","u274409976_certificados","Redes72607251$$$");
+            }
+            
+            return $conectar;
+        } catch (Exception $e) {
+            /*TODO: En Caso hubiera un error en la cadena de conexion */
+            print "¡Error BD!: " . $e->getMessage() . "<br/>";
+            die();
         }
-
     }
+
+    /*TODO: Para impedir que tengamos problemas con las ñ o tildes */
+    public function set_names(){
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+
+    /*TODO: Ruta principal del proyecto */
+    public static function ruta(){
+        // Detectar si estamos en entorno de desarrollo o producción
+        $host = $_SERVER['HTTP_HOST'];
+
+        if($host == 'localhost' || $host == '127.0.0.1') {
+            // Desarrollo
+            return "http://localhost/certificados/";
+        } else {
+            // Producción
+            return "https://peruaprende.com/certificados/";
+        }
+    }
+
+}
 ?>
